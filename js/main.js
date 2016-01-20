3cbe397998cd5d3af36eb4012b109a720906cff4
Shahid Bashir <shahid.chodhry@gmail.com>
2016-01-21 02:16:46 +0500

    new page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@ $('#tags').tagsInput( {
 	'height':'50px',
    	'width':'100%'
 } );
+
+	$('.preject-window').on('click', function(event) {
+		event.preventDefault();
+		$('#add_new').modal('hide');
+		$('#add_new_workspace').modal('show');
+	});
+	
 	
 	
 });//on laod
